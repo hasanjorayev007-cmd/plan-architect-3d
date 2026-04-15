42afc300c4a21530a87b3d680450dfdbe06f6bdf
hasanjorayev007-cmd <hasanjorayev@github.com>
2026-04-16 00:05:15 +0500

    Auto-deploy: setup Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/plan-architect-3d/",
   server: {
     host: "::",
     port: 8080,
